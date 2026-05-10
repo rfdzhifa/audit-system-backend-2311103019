eb3b194f7ef02fb661d10bd2ce0350ee1d3ed1d5
rfdzhifa <rfdnazhifah@gmail.com>
2026-05-10 23:43:00 +0700

    feat: audit logs complaints data

--- src/controllers/complaintControllers.js
+++ src/controllers/complaintControllers.js
@@ -1,6 +1,8 @@
 //IMPORT
 const prisma = require("../config/prisma")
 const { Prisma } = require("@prisma/client")
+const createAuditLog = require("../utils/createAuditLog")
+
 
 const createComplaint = async (req, res) => {
   try {
@@ -16,6 +18,26 @@ const createComplaint = async (req, res) => {
         description,
         userId: req.user.id
       }
+    })
+
+    //AUDIT LOG
+    await createAuditLog({
+      userId: req.user.id,
+      action: "CREATE",
+      entity: "COMPLAINT",
+      entityId: complaint.id,
+
+      endpoint: req.originalUrl,
+      method: req.method,
+
+      newData: complaint,
+
+      status: "SUCCESS",
+
+      riskLevel: "LOW",
+
+      ipAddress: req.ip,
+      userAgent: req.headers["user-agent"]
     })
 
     res.status(201).json({
@@ -87,6 +109,14 @@ const updateComplaint = async (req, res) => {
     const { id } = req.params
     const { response, status } = req.body
 
+    // get old data
+    const oldComplaint = await prisma.complaint.findUnique({
+      where: {
+        id: Number(id)
+      }
+    })
+
+    // update complaint
     const complaint = await prisma.complaint.update({
       where: {
         id: Number(id)
@@ -97,10 +127,34 @@ const updateComplaint = async (req, res) => {
       }
     })
 
+    // audit log
+    await createAuditLog({
+      userId: req.user.id,
+
+      action: "UPDATE",
+
+      entity: "COMPLAINT",
+      entityId: complaint.id,
+
+      endpoint: req.originalUrl,
+      method: req.method,
+
+      oldData: oldComplaint,
+      newData: complaint,
+
+      status: "SUCCESS",
+
+      riskLevel: "MEDIUM",
+
+      ipAddress: req.ip,
+      userAgent: req.headers["user-agent"]
+    })
+
     res.status(200).json({
       success: true,
       data: complaint
     })
+
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -113,16 +167,47 @@ const deleteComplaint = async (req, res) => {
   try {
     const { id } = req.params
 
+    // ambil data sebelum dihapus
+    const complaint = await prisma.complaint.findUnique({
+      where: {
+        id: Number(id)
+      }
+    })
+
+    // delete complaint
     await prisma.complaint.delete({
       where: {
         id: Number(id)
       }
     })
 
+    // create audit log
+    await createAuditLog({
+      userId: req.user.id,
+
+      action: "DELETE",
+
+      entity: "COMPLAINT",
+      entityId: Number(id),
+
+      endpoint: req.originalUrl,
+      method: req.method,
+
+      oldData: complaint,
+
+      status: "SUCCESS",
+
+      riskLevel: "HIGH",
+
+      ipAddress: req.ip,
+      userAgent: req.headers["user-agent"]
+    })
+
     res.status(200).json({
       success: true,
       message: "Complaint deleted"
     })
+
   } catch (error) {
     res.status(500).json({
       success: false,
