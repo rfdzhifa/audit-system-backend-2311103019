fe923c92753f0a61d8f22850aea07de5e49e6147
rfdzhifa <rfdnazhifah@gmail.com>
2026-05-11 11:38:43 +0700

    feat: added auth validation &  update seeder

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -12,11 +12,15 @@ const {
     deleteMyAccount,
     deleteUserByAdmin,
 } = require("../controllers/userControllers")
+const {
+  registerValidation,
+  loginValidation
+} = require("../validations/authValidation")
 const authMiddleware = require("../middleware/authMiddleware");
 
 //PUBLIC
-router.post("/login", loginUser)
-router.post("/register", createUser);
+router.post("/login", loginValidation, loginUser)
+router.post("/register", registerValidation, createUser);
 
 //NEED AUTH
 router.get("/me", authMiddleware, getProfile);
